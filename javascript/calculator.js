74bbd05e676085b2cd43ec8f9a515cc658d75b80
michaels107 <michaels.107@osu.edu>
2020-06-30 23:34:39 -0400

    Added the multiply functionality and added multiply to solve.

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,4 +1,5 @@
 /*Created 6/30/2020 by Duytan Tran
+* Edited 6/30/2020 by Sean Michaels : Added the solve button and added a multiply button to work.
 * Javascript code for calculator action listening events.*/
 
 /*Created 6/30/2020 by Duytan Tran
@@ -24,11 +25,21 @@ const calculator = document.querySelector('.calculator');
 const prepared_display = document.querySelector('.prepared_display');
 const display = document.querySelector('.display');
 const buttons = calculator.querySelector('.buttons');
+let op = "";
+let result = "";
+let first_num = "";
+let sec_num = "";
 buttons.addEventListener('click', e => {
     if(e.target.matches('button')){
         const button = e.target;
         const action = button.dataset.action;
         switch(action){
+            case 'multiply' :
+                first_num = display.textContent;
+                prepared_display.textContent = display.textContent + " *";
+                display.textContent = "0";
+                op = "*"; // 1 being a flag for multi
+                break;
             case 'subtract':
                 //stub
                 break;
@@ -36,10 +47,18 @@ buttons.addEventListener('click', e => {
                 clear_input(display, prepared_display);
                 break;
             case 'solve':
-                //stub
+                if(op == "*"){ // checks the flag for which op it is
+                        sec_num = display.textContent;
+                        result = parseInt(first_num) * parseInt(sec_num);
+                        first_num = "";
+                        sec_num = "";
+                        prepared_display.textContent = "";
+                        display.textContent = result.toString();
+                }
                 break;
             default:
                 digit_input(button, display, calculator);
+                let num = display.textContent;
         }
     }
 })
