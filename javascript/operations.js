9dff2a382426bc09456a538220f4c8b3da047190
CarolineWheeler <wheeler.1017@osu.edu>
2020-07-08 08:22:46 -0400

    changes to operations.js

--- javascript/operations.js
+++ javascript/operations.js
@@ -2,38 +2,39 @@
 Created 7/7/2020 by Caroline Wheeler
 Edited 7/8/20 by Reema Gupta
 
-Note - Arrow operator: => , allows for shorter function syntax.
+Note - Arrow : => , allows for shorter function syntax.
 */
-/*
-Changed regular expression so that it could take in negative numbers
-*/
-const regrex="(-)?(\d\.\d|\d)"
+
+//Changed regular expression so that it could take in negative numbers - Reema Gupta
+const regex="(-)?(\d\.\d|\d)";
+
 /*
 Created 7/7/2020 by Caroline Wheeler
 Takes a string(sym) and a function(operation) and
 returns an object with two properties (operation & find matches).
 The value of both properties is a function.
  */
-
-const regrex="(-)?(\d\.\d|\d)";
 const CreateSimpleOperation = (sym, op) => ({
     // findMatches takes a string as a param and finds all matches of a regular expression.
     findMatches: (str) =>
-        Array.from(
-            new Set(
-                //Create a regular expression which takes the given symbol between two floats/ints.
-                //match returns null if no matches, so if false return empty array.
-                str.match(new RegExp( regex+ "\\" + sym + regex, "g")) || [] )),
-
+        //Create a regular expression which takes the given symbol between two floats/ints.
+        Array.from(new Set(str.match(new RegExp( regex+ "\\" + sym + regex, "g")))),
     // operation takes a string as a param, splits at the symbol into an array containing numbers
     evaluate: (match) => op(match.split(sym).map(parseFloat)),
 });
 
 /*
+Reema does trig stuff here.
+ */
+
+/*
 Created 7/7/2020 by Caroline Wheeler
-An Array that holds objects from calling createSimpleOperation.
+An Array that holds objects creating from calling functions to create.
+These are in the correct order of operations.
  */
 const operations = [
+    // Reema - create trig operations here
+    // any other additional operations just need to be in the correct order
     CreateSimpleOperation("*", ([a, b]) => a * b),
     CreateSimpleOperation("/", ([a, b]) => a / b),
     CreateSimpleOperation("+", ([a, b]) => a + b),
@@ -41,7 +42,26 @@ const operations = [
 ];
 
 /*
+Created 7/8/2020 by Caroline Wheeler
+Takes a string (ex. "2 * 4 -1") and returns true if it contains no math expression to evaluate.
+ */
+const noMatches = (str) => {
+    return operations.every(({ findMatches }) => findMatches(str).length === 0)
+}
+
+/*
+Created 7/8/2020 by Caroline Wheeler
+Takes a string & an object containing 2 functions.
+Returns a string similar to `str` but with all instances of the given
+operation having been replaced with their evaluated result.
+ */
+const evaluateOperation = (str, operation) =>
+    operation.findMatches(str).reduce((a, match) => a.replace(match, operation.evaluate(match)), str );
+
+
+/*
 Created 7/7/2020 by Caroline Wheeler
+Edited 7/8/2020 by Caroline Wheeler - split up into multiple functions.
 Takes a string (ex. "2 + 4 * 6") and returns an evaluation.
  */
 const evaluateMath = (mathString) => {
@@ -49,23 +69,13 @@ const evaluateMath = (mathString) => {
     const withoutSpaces = mathString.split(" ").join("");
 
     // if there are no matches for any of the operations then return the input - end recursion
-    if (operations.every(({ findMatches }) => findMatches(withoutSpaces).length === 0)) {
-        return withoutSpaces;
+    if (noMatches(withoutSpaces)) {
+        return mathString;
     }
 
-    // reduction
-    const result = operations.reduce((accum, operation) => {
-        const matches = operation.findMatches(accum);
-        const allMatches = matches.reduce((accum, match) => {
-            const matchEvaluated = operation.evaluate(match);
-            const newResult = accum.replace(match, matchEvaluated);
-            return newResult;
-        }, accum);
-        return allMatches;
-    }, withoutSpaces);
-
-    //recursively call evaluateMath with reduced operations until if statement returns false.
+    const result = operations.reduce(evaluateOperation, withoutSpaces);
     return evaluateMath(result);
+
 };
 
 
