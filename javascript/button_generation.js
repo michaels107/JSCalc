bb89a4eb857ee7191882c14d085c1810bcb27a5c
Duytan Tran <tran.732@osu.edu>
2020-07-08 14:17:07 -0400

    added listener to all non-digits/non-sign buttons so that a new digit input can be taken upon an operation is pressed

--- javascript/button_generation.js
+++ javascript/button_generation.js
@@ -1,9 +1,13 @@
 /*Created 7/3/2020 by Duytan Tran
 * Edited 7/7/2020 by Sean Michaels
-* Edited 7/8/2020 by Duytan Tran: Additional documentation
+* Edited 7/8/2020 by Duytan Tran: Additional documentation, added listener to non-digits/non-sign buttons
 * Javascript function for the generation of calculator buttons*/
 
+const digits = document.getElementsByName("digits")[0];
+
 /*Created 7/3/2020 by Duytan Tran
+* Edited 7/8/2020 by Duytan Tran: added listener to non-digits/non-sign buttons so that new digit inputs can be had
+*                                 instead of always appending
 *
 * Adds button at designated location in html tree with appropriate attributes,
 * content, and event listener.
@@ -17,6 +21,13 @@ function developButton(parentName, content, name, listenerFunction){
     button.innerHTML = content;
     button.setAttribute("name", name);
     button.addEventListener("click", listenerFunction);
+
+    // Allows a new digit input to be had if a non-digit/sign button was pressed
+    if(name != 'digit' && name != 'sign'){
+        button.addEventListener("click", e =>{
+                digits.newInput = true;
+        });
+    }
     parent.appendChild(button);
 }
 
--- javascript/digits.js
+++ javascript/digits.js
@@ -1,11 +1,13 @@
 /*Created 7/3/2020 by Duytan Tran
+* Edited 7/8/2020 by Duytan Tran: added digits.newInput conditional to avoid appending digits after operations
 * Relevant logic and generation for all 10 calculator digit buttons*/
 for(let i = 9; i >= 0; i--)
 {
     developButton("digits", i, "digit", e => {
         const display = document.getElementsByClassName("display")[0];
-        if(display.innerHTML == "0"){
+        if(display.innerHTML == "0" || digits.newInput){
             display.innerHTML = e.target.textContent;
+            digits.newInput = false;
         } else {
             display.innerHTML += e.target.textContent;
         }
